c610d5a4714f23f23fdc263a3cd296515dfd673c
TonyDev21 <tonysalinasdev@gmail.com>
2025-07-31 17:58:20 -0500

    style product-details

--- src/components/atomic/Button.tsx
+++ src/components/atomic/Button.tsx
@@ -2,13 +2,14 @@ interface ButtonProps {
   label: string;
   handleClick: () => void;
   disabled: boolean;
+  width?: string;
 }
 
-const Button = ({ label, handleClick , disabled}: ButtonProps) => {
+const Button = ({ label, handleClick , disabled,width}: ButtonProps) => {
   return (
     <button
       onClick={handleClick}
-      className={`w-2/5  p-2 rounded-3xl text-xl md:text-lg lg:px-0 lg:text-base ${disabled ?'bg-gray-400 cursor-not-allowed': 'bg-black cursor-pointer text-white'}  `}
+      className={`${width}  p-2 rounded-3xl text-xl md:text-lg lg:px-0 lg:text-base ${disabled ?'bg-gray-400 cursor-not-allowed': 'bg-black cursor-pointer text-white hover:scale-105'} transition-transform duration-200 ease-in-out`}
     >
       {label}
     </button>
--- src/components/pages/ProductDetail.tsx
+++ src/components/pages/ProductDetail.tsx
@@ -5,6 +5,9 @@ import Navbar from "../moleculs/Navbar";
 import Button from "../atomic/Button";
 import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
+import { LuShield } from "react-icons/lu";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { IoReload } from "react-icons/io5";
 
 const ProductDetail = () => {
 
@@ -34,13 +37,34 @@ const ProductDetail = () => {
   console.log(context.cartItems, "Items seleccionados.");
 
   return (
-    <div>
+    <div className="container mx-auto">
       <Navbar />
-      <h1>{data?.title}</h1>
-      <img src={data?.image} alt={data?.title} className="max-w-[150px]" />
-      <p>{data?.description}</p>
-      <Button label="Agregar al carrito" handleClick={handleClick} disabled={isDisabled}/>
-      <p>Elementos agregados: {context.count}</p>
+      <div className="w-5/6 mx-auto h-auto block lg:flex">
+        <div className="w-full lg:w-1/2 h-[550px] lg:p-8">
+          <img src={data?.image} alt={data?.title} className="w-full h-full object-fill" />
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="p-4 space-y-3">
+            <h4 className="text-2xl">{data?.category}</h4>
+            <h1 className="text-3xl font-bold">{data?.title}</h1>
+            <h2 className="text-3xl font-bold">${data?.price}</h2>
+          </div>
+          <div className="p-4">
+            <h3 className="text-2xl font-medium">Features</h3>
+            <div className="text-2xl mt-4">
+              <p className="flex items-center"><span className="inline-block pr-2 text-green-700"><LuShield /></span>2 year warranty included</p>
+              <p className="flex items-center"><span className="inline-block pr-2 text-blue-700"><CiDeliveryTruck /></span>Free shipping worldwide</p>
+              <p className="flex items-center"><span className="inline-block pr-2 text-purple-700"><IoReload /></span>30-day return policy</p>
+            </div>
+          </div>
+          <Button label="Agregar al carrito" handleClick={handleClick} disabled={isDisabled} width="w-full py-4"/>
+          <p className="text-center p-3 font-light">Free shipping on orders over $100</p>
+        </div>
+      </div>
+      <div className="w-5/6 mx-auto h-auto mt-10 p-4 mb-4">
+          <h4 className="text-3xl font-medium mb-4">Product Details</h4>
+          <p className="text-xl">{data?.description}</p>
+        </div>
     </div>
   );
 };
